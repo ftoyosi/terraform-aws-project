f5e1b40c23af4ae568143d9c4935da555b015031
Olufisayo <ftoyosi@yahoo.com>
2024-09-20 16:47:37 +0200

    using terraform s3 module

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
-resource "aws_s3_bucket" "example" {
-  bucket_prefix = "my-tf-test-bucket-prefix"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+module "s3" {
+  source         = "github.com/ftoyosi/terraform-aws-s3-module"
+  s3_bucket_name = "dob-sep-bucket-oajewoleeee"
 }
-
-# Hands-on 4: use the module from the repo terraform-aws-s3-module
