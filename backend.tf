4109529105fe254d25f601c36737240cba4c3e5f
Olufisayo <ftoyosi@yahoo.com>
2024-09-20 15:33:35 +0200

    backend files for terraform state

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # Hands-on 2: replace <alias> with your alias, e.g. terraform-backend-dob-elgama
-    bucket = "terraform-backend-dob-oajewole"
+    bucket = "terraform-backend-dob-oajewoleeee"
     key    = "terraform-aws-project"
     region = "eu-central-1"
   }
